13496611620f24014471360282bebdab25f0905f
karripar <karripartanen25@gmail.com>
2025-02-26 10:37:00 +0200

    update

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -149,39 +149,7 @@ const Profile = () => {
 
         ))}
       </div>
-          <h2 className="text-3xl font-bold text-gray-800">{user?.username}</h2>
-
-          {/* User Role */}
-          <p className="text-gray-600 text-lg">{user?.level_name}</p>
-
-          {/* Media or Posts Section */}
-          <div className="flex space-x-6">
-            <div className="text-center">
-              <p className="text-sm text-gray-500">Posts</p>
-              <p className="font-semibold text-xl">15</p>
-            </div>
-            <div className="text-center">
-              <p className="text-sm text-gray-500">Followers</p>
-              <p className="font-semibold text-xl">200</p>
-            </div>
-            <div className="text-center">
-              <p className="text-sm text-gray-500">Following</p>
-              <p className="font-semibold text-xl">180</p>
-            </div>
-          </div>
-
-          {/* Action buttons */}
-          <div className="w-full mt-4 flex justify-center space-x-4">
-            <button className="px-6 py-2 bg-blue-500 text-white rounded-full font-semibold hover:bg-blue-600 transition duration-200">
-              Edit Profile
-            </button>
-            <Link to="/logout">
-              <button className="px-6 py-2 border-2 border-gray-300 text-gray-700 rounded-full font-semibold hover:bg-gray-200 transition duration-200">
-                Logout
-              </button>
-            </Link>
-          </div>
-        </div>
+    </div>
 
       <div className="flex flex-col justify-center min-h-1/2 bg-gray-900 p-4 my-20">
         <h2 className="text-2xl font-semibold text-gray-400 text-center mb-6">
